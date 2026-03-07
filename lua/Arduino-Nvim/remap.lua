79b31931206be95618a273c2d8e2e2545d5dd888
Good1Cheese <map.omebauw@erine.email>
2026-03-07 14:12:37 +0300

    chore: format

--- lua/Arduino-Nvim/remap.lua
+++ lua/Arduino-Nvim/remap.lua
@@ -1,11 +1,11 @@
-vim.keymap.set("n", "<Leader>au", ":InoUpload<CR>", { silent = true })  -- Upload code
-vim.keymap.set("n", "<Leader>ac", ":InoCheck<CR>", { silent = true })   -- Compile/check code
-vim.keymap.set("n", "<Leader>as", ":InoStatus<CR>", { silent = true })  -- Show current board and port status
-vim.keymap.set("n", "<Leader>ag", ":InoGUI<CR>", { silent = true })     -- Open GUI for setting board and port
+vim.keymap.set("n", "<Leader>au", ":InoUpload<CR>", { silent = true })      -- Upload code
+vim.keymap.set("n", "<Leader>ac", ":InoCheck<CR>", { silent = true })       -- Compile/check code
+vim.keymap.set("n", "<Leader>as", ":InoStatus<CR>", { silent = true })      -- Show current board and port status
+vim.keymap.set("n", "<Leader>ag", ":InoGUI<CR>", { silent = true })         -- Open GUI for setting board and port
 vim.keymap.set("n", "<Leader>am", ":InoMonitor<CR>", { silent = true })     -- Open Serial monitor with default port and baud rate
 vim.keymap.set("n", "<Leader>al", ":InoLib<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>ab", ":InoSelectBoard<CR>", { silent = true })     -- open board selection gui
-vim.keymap.set("n", "<Leader>ap", ":InoSelectPort<CR>", { silent = true }) -- open board selection gui
+vim.keymap.set("n", "<Leader>ab", ":InoSelectBoard<CR>", { silent = true }) -- open board selection gui
+vim.keymap.set("n", "<Leader>ap", ":InoSelectPort<CR>", { silent = true })  -- open board selection gui
 vim.keymap.set("n", "<Leader>ar", ":InoUploadReset<CR>", { silent = true }) -- Upload with manual reset for UNO R4 WiFi
 
 
@@ -15,4 +15,4 @@ vim.keymap.set("n", "<Esc>", function()
             vim.api.nvim_win_close(win, false)
         end
     end
-end, { silent = true , noremap = true })
+end, { silent = true, noremap = true })
